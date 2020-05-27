23b2cb04d9f3a2b2a51cdff8e6c65624abc46919
Ajay Kumar <ajaykumar.vvit@gmail.com>
2020-05-27 12:45:03 +0530

    Basic react components, class component, function component, intro to state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sample from './Sample';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  let names = ['Steve', 'Mark', 'Maverick'];
+  return (    
+    <div>
+      <Home></Home>
+      <h4> From defult App function Component</h4>
+      <ul>
+        { names.map((n) => (
+          <li> {n} </li>
+        ))}
+      </ul>
+      <Sample></Sample>
     </div>
   );
+}
+
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Ajay Kumar'
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h3> From Home class component </h3>
+        <h4> Name from state: {this.state.name}</h4>
+      </div>
+    );      
+  }
 }
 
 export default App;
---
+++ src/Sample.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Sample extends React.Component {
+    render() {
+        return <h4> From another Sample component - From another file</h4>;
+    }
+}
+
+export default Sample;
